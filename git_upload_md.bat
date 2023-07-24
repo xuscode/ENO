a36bf4c03f5419ad54a30e8d0217412d1486c98d
song_hsu <song_hsu@163.com>
2023-07-24 10:25:06 +0800

    update

--- git_upload_md.bat
+++ git_upload_md.bat
@@ -1,4 +1,4 @@
-set date=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+@REM set date=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 echo %data%
 
 @echo off
@@ -9,7 +9,7 @@ git add *.md
 
 @echo WRITE UPDATE AND PRESS ENTER:
 ::set /p GetYourLog=
-git commit -m "%date%"
+git commit -m "%date% %time%"
 git push
 
 
